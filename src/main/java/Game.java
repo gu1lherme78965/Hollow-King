4499d4c32caf9b1da55536ed6de9ecb26ec5ad94
Guilherme Pires Figueiredo <up202406001@up.pt>
2025-11-09 23:21:50 +0000

    Basic character movement implemented

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,14 +1,18 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class Game {
     private Screen screen;
+    private int x, y;
 
     Game() {
         TerminalSize size = new TerminalSize(150, 50);
@@ -23,11 +27,14 @@ public class Game {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        x=10;
+        y=10;
     }
 
     public void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
 
@@ -35,10 +42,40 @@ public class Game {
         try {
             while(true) {
                 draw();
+
+                // user input reading
+                KeyStroke key = screen.readInput();
+                if(key.getKeyType() == KeyType.EOF) {break;}
+                processKey(key);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    private void processKey(KeyStroke key) throws IOException{
+        if (key.getKeyType() == KeyType.Character) {
+            switch (key.getCharacter()) {
+                case 'q':
+                    screen.close();
+                    break;
+            }
+        } else {
+            switch (key.getKeyType()) {
+                case KeyType.ArrowLeft:
+                    x--;
+                    break;
+                case KeyType.ArrowRight:
+                    x++;
+                    break;
+                case KeyType.ArrowUp:
+                    y--;
+                    break;
+                case KeyType.ArrowDown:
+                    y++;
+                    break;
+            }
+        }
     }
 }
